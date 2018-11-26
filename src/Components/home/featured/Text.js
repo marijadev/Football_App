589da8a85250f0e07476ebd7096ec4bb0a9d47d6
Marija Kovacevic <Marija.Kovacevic@endava.com>
2018-11-26 09:19:28 +0100

    add player img

--- src/Components/home/featured/Text.js
+++ src/Components/home/featured/Text.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { easePolyOut } from 'd3-ease';
 import Animate from 'react-move/Animate';
+import FeaturedPlayer from '../../../Resources/images/featured_player.png';
 
 class Text extends Component {
 	animateNumber = () => (
@@ -22,7 +23,7 @@ class Text extends Component {
 				)
 			} }
 		</Animate>
-	)
+	);
 
 	animateFirst = () => (
 		<Animate
@@ -45,13 +46,60 @@ class Text extends Component {
 				)
 			} }
 		</Animate>
-	)
+	);
+
+	animateSecond = () => (
+		<Animate
+			start={ {
+				opacity: [ 0 ],
+				x: [ 503 ],
+				y: [ 586 ]
+			} }
+			enter={ {
+				opacity: [ 1 ],
+				x: [ 273 ],
+				y: [ 586 ],
+				timing: { delay: 300, duration: 500, ease: easePolyOut }
+			} }
+		>
+
+			{ ( { opacity, x, y, timing } ) => {
+				return (
+					<div className="featured_second" style={ { opacity, transform: `translate(${ x }px, ${ y }px)` } }>Championship</div>
+				)
+			} }
+		</Animate>
+	);
+
+	animatePlayer = () => (
+		<Animate
+			start={ {
+				opacity: [ 0 ],
+			} }
+			enter={ {
+				opacity: [ 1 ],
+				timing: { delay: 800, duration: 500, ease: easePolyOut }
+			} }
+		>
+
+			{ ( { opacity } ) => {
+				return (
+					<div className="featured_player" style={ { opacity, background: `url(${ FeaturedPlayer })`, transform: `translate(550px, 201px)` } }>
+
+					</div>
+				)
+			} }
+		</Animate>
+	);
+
 
 	render() {
 		return (
 			<div className="featured_text">
+				{ this.animatePlayer() }
 				{ this.animateNumber() }
 				{ this.animateFirst() }
+				{ this.animateSecond() }
 			</div>
 		)
 	}
