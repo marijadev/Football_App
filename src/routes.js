46f8ab1fd70949e874012c19ca7f4ff3c66ab0c6
Marija Kovacevic <Marija.Kovacevic@endava.com>
2018-11-29 13:13:37 +0100

    add team component

--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,7 @@ import PublicRoute from './Components/authRoutes/publicRoutes';
 
 import Home from './Components/home';
 import SignIn from './Components/signin';
+import TheTeam from './Components/theTeam';
 
 import Dashboard from './Components/admin/Dashboard';
 import AdminMatches from './Components/admin/matches';
@@ -27,6 +28,7 @@ const Routes = ( props ) => {
 					<PrivateRoute { ...props } path="/admin_matches" exact component={ AdminMatches } />
 					<PrivateRoute { ...props } path="/dashboard" exact component={ Dashboard } />
 					<PublicRoute { ...props } restricted={ true } path="/sign_in" exact component={ SignIn } />
+					<PublicRoute { ...props } restricted={ false } path="/the_team" exact component={ TheTeam } />
 					<PublicRoute { ...props } restricted={ false } path="/" exact component={ Home } />
 				</Switch>
 			</Layout>
